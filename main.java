6843520f8b9fb19d9d2a3645611d0cadaa2308d0
vista747 <julius@julius.cool>
2025-11-15 16:35:59 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,3 +1,41 @@
+import java.util.Scanner;
+
 public class main {
-    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Press W, A, S, or D (press Q to quit):");
+
+        while (true) {
+            String input = scanner.nextLine().toLowerCase();
+
+            if (input.equals("q")) {
+                System.out.println("Goodbye!");
+                break;
+            }
+
+            switch (input) {
+                case "w":
+                    //put code here
+                    break;
+
+                case "a":
+                    //put code here
+                    break;
+
+                case "s":
+                    //put code here
+                    break;
+
+                case "d":
+                    //put code here
+                    break;
+
+                default:
+            }
+        }
+
+        scanner.close();
+    }
 }
